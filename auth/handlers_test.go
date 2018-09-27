8c1701cb57f26cdb211a5d09f2d820336c25b8d5
JordanDeBeer <jordan@jordandebeer.com>
2018-09-26 20:31:21 -0400

    add /refresh test

--- auth/handlers_test.go
+++ auth/handlers_test.go
@@ -197,3 +197,47 @@ func TestAuthMw(t *testing.T) {
 	}
 
 }
+
+// test /refresh
+func TestRefresh(t *testing.T) {
+	loginReq, err := http.NewRequest("POST", "/login", strings.NewReader(`{"username":"jordan","password":"test"}`))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	lrr := httptest.NewRecorder()
+	loginHandler := http.Handler(s.loginHandler())
+	loginHandler.ServeHTTP(lrr, loginReq)
+	resp := make(map[string]interface{})
+	json.Unmarshal([]byte(lrr.Body.String()), &resp)
+
+	refresh_token := resp["refresh_token"]
+
+	req, err := http.NewRequest("GET", "/refresh", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// We create a ResponseRecorder (which satisfies http.ResponseWriter) to record the response.
+	rr := httptest.NewRecorder()
+	handler := http.Handler(s.refreshHandler())
+
+	// Our handlers satisfy http.Handler, so we can call their ServeHTTP method
+	// directly and pass in our Request and ResponseRecorder.
+	req.Header = map[string][]string{
+		"Authorization": {fmt.Sprintf("Bearer %s", refresh_token)},
+	}
+	handler.ServeHTTP(rr, req)
+
+	// Check the status code is what we expect.
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
+	}
+
+	// Check the response body is what we expect.
+	access_token, err := VerifyJwt(resp["access_token"].(string), s.privKey.Public())
+	if err != nil {
+		t.Errorf("handler returned an invalid access token. token: %s", access_token)
+	}
+
+}
